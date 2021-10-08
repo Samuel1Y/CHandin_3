04210f331bd79b6445727df7986a02a834764bab
samuel1Y <samuel.bajzik0@gmail.com>
2021-10-08 18:41:37 +0200

    Added Login

--- WebApplication/obj/Debug/net5.0/RazorDeclaration/Pages/FetchData.razor.g.cs
+++ WebApplication/obj/Debug/net5.0/RazorDeclaration/Pages/FetchData.razor.g.cs
@@ -13,78 +13,85 @@ namespace WebApplication.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\terez\RiderProjects\CHandIn1\WebApplication\_Imports.razor"
+#line 1 "C:\Users\samue\RiderProjects\CHandInnew\WebApplication\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\terez\RiderProjects\CHandIn1\WebApplication\_Imports.razor"
+#line 2 "C:\Users\samue\RiderProjects\CHandInnew\WebApplication\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\terez\RiderProjects\CHandIn1\WebApplication\_Imports.razor"
+#line 3 "C:\Users\samue\RiderProjects\CHandInnew\WebApplication\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\terez\RiderProjects\CHandIn1\WebApplication\_Imports.razor"
+#line 4 "C:\Users\samue\RiderProjects\CHandInnew\WebApplication\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\terez\RiderProjects\CHandIn1\WebApplication\_Imports.razor"
+#line 5 "C:\Users\samue\RiderProjects\CHandInnew\WebApplication\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\terez\RiderProjects\CHandIn1\WebApplication\_Imports.razor"
+#line 6 "C:\Users\samue\RiderProjects\CHandInnew\WebApplication\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\terez\RiderProjects\CHandIn1\WebApplication\_Imports.razor"
+#line 7 "C:\Users\samue\RiderProjects\CHandInnew\WebApplication\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\terez\RiderProjects\CHandIn1\WebApplication\_Imports.razor"
+#line 8 "C:\Users\samue\RiderProjects\CHandInnew\WebApplication\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\terez\RiderProjects\CHandIn1\WebApplication\_Imports.razor"
+#line 9 "C:\Users\samue\RiderProjects\CHandInnew\WebApplication\_Imports.razor"
 using WebApplication;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\terez\RiderProjects\CHandIn1\WebApplication\_Imports.razor"
+#line 10 "C:\Users\samue\RiderProjects\CHandInnew\WebApplication\_Imports.razor"
 using WebApplication.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\terez\RiderProjects\CHandIn1\WebApplication\Pages\FetchData.razor"
+#line 4 "C:\Users\samue\RiderProjects\CHandInnew\WebApplication\Pages\FetchData.razor"
 using WebApplication.Data;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 2 "C:\Users\samue\RiderProjects\CHandInnew\WebApplication\Pages\FetchData.razor"
+           [Authorize(Policy = "MustBeAdmin")]
 
 #line default
 #line hidden
@@ -98,7 +105,7 @@ using WebApplication.Data;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 41 "C:\Users\terez\RiderProjects\CHandIn1\WebApplication\Pages\FetchData.razor"
+#line 42 "C:\Users\samue\RiderProjects\CHandInnew\WebApplication\Pages\FetchData.razor"
        
     private WeatherForecast[] forecasts;
 
