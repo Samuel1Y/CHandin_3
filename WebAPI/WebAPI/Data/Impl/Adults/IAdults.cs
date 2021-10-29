a536b1c2355e0320f227a642aa8bd3df58463508
Samuel1Y <79629901+Samuel1Y@users.noreply.github.com>
2021-10-29 22:11:23 +0200

    WEB API

---
+++ WebAPI/WebAPI/Data/Impl/Adults/IAdults.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Models;
+
+namespace WebApplication.Data.Impl.Adults
+{
+    public interface IAdults
+    {
+        Task<IList<Adult>> GetAdultsAsync();
+        Task<Adult>   AddAdultAsync(Adult adult);
+        Task   RemoveAdultAsync(int todoId);
+        Task<Adult> UpdateAdultAsync(Adult adult);
+    }
+}
