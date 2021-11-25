eef92d04c264f65e4a44e0fda10ff407d2e8b9fb
samuel1Y <samuel.bajzik0@gmail.com>
2021-11-25 03:13:22 +0100

    db done, some methods + added data

--- WebAPI/DataAccess/DBContext.cs
+++ WebAPI/DataAccess/DBContext.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Models;
 using WebApplication.Models;
 
@@ -11,4 +15,71 @@ public class DBContext : DbContext
         // name of database
         optionsBuilder.UseSqlite(@"Data Source = C:\Users\samue\Documents\VIA\3rd Semester\DNP\HandIn_3\Adultsdotn.db");
     }
+
+    public async Task<IList<Adult>> getAllAdults() //to list all adults
+    {
+        IQueryable<Adult> asQueryable = Adults.AsQueryable();
+        return await asQueryable.ToListAsync();
+    }
+    
+    public async Task<IList<Adult>> getAdult(int? id, string? FirstName, string? LastName,
+         string? HairColor, string? EyeColor, int? age,
+         int? Weight, int? height, string? Sex)
+    {
+        IQueryable<Adult> asQueryable = Adults.AsQueryable();
+        if (id != null)
+        {
+            asQueryable = asQueryable.Where(adult => adult.Id == id);
+        }
+        if (FirstName != null)
+        {
+            asQueryable = asQueryable.Where(adult => adult.FirstName == FirstName);
+        }
+        if (LastName != null)
+        {
+            asQueryable = asQueryable.Where(adult => adult.LastName == LastName);
+        }
+        if (HairColor != null)
+        {
+            asQueryable = asQueryable.Where(adult => adult.HairColor == HairColor);
+        }
+        if (EyeColor != null)
+        {
+            asQueryable = asQueryable.Where(adult => adult.EyeColor == EyeColor);
+        }
+        if (age != null)
+        {
+            asQueryable = asQueryable.Where(adult => adult.Age == age);
+        }
+        if (Weight != null)
+        {
+            asQueryable = asQueryable.Where(adult => adult.Weight == Weight);
+        }
+        if (height != null)
+        {
+            asQueryable = asQueryable.Where(adult => adult.Height == height);
+        }
+        if (Sex != null)
+        {
+            asQueryable = asQueryable.Where(adult => adult.Sex == Sex);
+        }
+        return await asQueryable.ToListAsync();
+    }
+    
+    public async Task RemoveAdultAsync(int id)
+    {
+        Adult toRemove = Adults.First(t => t.Id == id);
+        Adults.Remove(toRemove);
+    }
+    
+    public async Task<IList<User>> getUser(string? username) //for login
+    {
+        IQueryable<User> asQueryable = Users.AsQueryable();
+        if (username != null)
+        {
+            asQueryable = asQueryable.Where(user => user.UserName == username);
+        }
+        return await asQueryable.ToListAsync();
+    }
+
 }
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -6,14 +6,61 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Models;
+using WebApplication.Models;
 
 namespace WebAPI
 {
+    /*
+     *  some methods in dbContext, idk if it's the correct that way
+     *  3 Adultsdotn files are for the db, maybe you will need to change path in dbContext
+     *  Last thing to do is update controllers to call methods from db I guess ?
+     *  pls delete this comment after reading :)
+     */
     public class Program
     {
         public static void Main(string[] args)
         {
+            //AddUser();
+            //AddAdult();
             CreateHostBuilder(args).Build().Run();
+            
+        }
+        
+        private static void AddUser()
+        {
+            User user = new User()
+            {
+                UserName = "user1",
+                Password = "password1",
+                Role = "Admin"
+            };
+            using (DBContext lb = new DBContext())
+            {
+                lb.Users.Add(user);
+                lb.SaveChanges();
+            }
+        }
+        
+        private static void AddAdult()
+        {
+            Adult adult2 = new Adult()
+            {
+                FirstName = "Mona",
+                LastName = "Paimon",
+                HairColor = "Blue",
+                EyeColor = "blue",
+                Age = 25,
+                Weight = 47,
+                Height = 162,
+                Sex = "F",
+                JobTitle = {JobTitle = "Teacher", Salary = 98742}
+            };
+            using (DBContext lb = new DBContext())
+            {
+                lb.Adults.Add(adult2);
+                lb.SaveChanges();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
